cd1a471d4d95e5f9d830d836546e5f587fb52e04
Berkay Arslan <berkayarslan9@gmail.com>
2024-03-12 15:13:10 +0300

    Add Review Service

---
+++ Final-Project/UserService/src/main/java/com/berkayarslan/UserService/service/ReviewService.java
@@ -0,0 +1,23 @@
+package com.berkayarslan.UserService.service;
+
+import com.berkayarslan.UserService.general.BaseEntityService;
+import com.berkayarslan.UserService.model.Review;
+import com.berkayarslan.UserService.repository.ReviewRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReviewService extends BaseEntityService<Review, ReviewRepository> {
+    protected ReviewService(ReviewRepository repository) {
+        super(repository);
+    }
+
+    public List<Review> findAllByRestaurantId(Long id){
+        return getRepository().findAllByRestaurantId(id);
+    }
+
+    public List<Review> findAllByUserId(Long id){
+        return getRepository().findAllByUser_Id(id);
+    }
+}
