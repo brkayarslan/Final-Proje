f0817d2ffdcbe4d32c5c8bd98bb844cbbd4f18f1
Berkay Arslan <berkayarslan9@gmail.com>
2024-03-12 12:11:53 +0300

    Add Base entity and service

---
+++ Final-Project/UserService/src/main/java/com/berkayarslan/UserService/general/BaseAdditionalFields.java
@@ -0,0 +1,17 @@
+package com.berkayarslan.UserService.general;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Embeddable
+@Getter
+@Setter
+public class BaseAdditionalFields {
+
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+
+}
---
+++ Final-Project/UserService/src/main/java/com/berkayarslan/UserService/general/BaseModel.java
@@ -0,0 +1,6 @@
+package com.berkayarslan.UserService.general;
+
+public interface BaseModel {
+
+    Long getId();
+}
