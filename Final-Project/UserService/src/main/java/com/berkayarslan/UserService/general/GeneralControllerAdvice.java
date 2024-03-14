40add07f9340acdbe482a7cf45c553c3ff694da8
Berkay Arslan <berkayarslan9@gmail.com>
2024-03-14 13:14:41 +0300

    Add GeneralControllerAdvice

---
+++ Final-Project/UserService/src/main/java/com/berkayarslan/UserService/general/GeneralControllerAdvice.java
@@ -0,0 +1,15 @@
+package com.berkayarslan.UserService.general;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class GeneralControllerAdvice extends ResponseEntityExceptionHandler {
+
+}
