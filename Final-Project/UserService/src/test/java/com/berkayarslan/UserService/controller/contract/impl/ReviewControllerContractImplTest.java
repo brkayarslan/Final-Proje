54b616d654d1a619845c55bc3d6a121ae4a837f6
Berkay Arslan <berkayarslan9@gmail.com>
2024-03-16 09:21:43 +0300

    Add User microservice Tests

---
+++ Final-Project/UserService/src/test/java/com/berkayarslan/UserService/controller/contract/impl/ReviewControllerContractImplTest.java
@@ -0,0 +1,102 @@
+package com.berkayarslan.UserService.controller.contract.impl;
+
+import com.berkayarslan.UserService.client.RestaurantClient;
+import com.berkayarslan.UserService.dto.ReviewDTO;
+import com.berkayarslan.UserService.mapper.ReviewConverter;
+import com.berkayarslan.UserService.model.Review;
+import com.berkayarslan.UserService.model.User;
+import com.berkayarslan.UserService.request.ReviewSaveRequest;
+import com.berkayarslan.UserService.request.ReviewUpdateRequest;
+import com.berkayarslan.UserService.service.ReviewScoreSenderService;
+import com.berkayarslan.UserService.service.ReviewService;
+import com.berkayarslan.UserService.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ReviewControllerContractImplTest {
+
+    @Mock
+    private ReviewService reviewService;
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private ReviewScoreSenderService reviewScoreSenderService;
+    @Mock
+    private ReviewConverter reviewConverter;
+
+    @InjectMocks
+    private ReviewControllerContractImpl reviewControllerContract;
+    @Test
+    void saveReview() {
+        ReviewSaveRequest reviewSaveRequest = new ReviewSaveRequest(1L,
+                                                                    "restaurantId",
+                                                                    5 ,
+                                                                    5 ,
+                                                                    5 ,
+                                                                    "comment");
+
+        User user = new User();
+        Review review =  new Review(user,
+                                    "restaurantId",
+                                    5 ,
+                                    5 ,
+                                    5 ,
+                                    "comment",
+                                    LocalDate.now());
+        Review savedReview = new Review(1L,
+                                         user,
+                                         "restaurantId",
+                                         5 ,
+                                         5 ,
+                                         5 ,
+                                         "comment",
+                                         LocalDate.now());
+        ReviewDTO expectedDTO  = new ReviewDTO(1L,
+                user.getFirstName(),
+                "restaurantId",
+                5 ,
+                5 ,
+                5 ,
+                "comment",
+                LocalDate.now());
+        when(userService.findByIdWithControl(1L)).thenReturn(user);
+        when(reviewConverter.convertToReview(user,reviewSaveRequest)).thenReturn(review);
+        when(reviewScoreSenderService.sendReviewScoresToRestaurant(review)).thenReturn(true);
+        when(reviewService.save(any(Review.class))).thenReturn(savedReview);
+
+
+        ReviewDTO resultDTO = reviewControllerContract.saveReview(reviewSaveRequest);
+
+        verify(userService).findByIdWithControl(reviewSaveRequest.userId());
+        verify(reviewService).save(review);
+    }
+
+    @Test
+    void updateReview() {
+        ReviewUpdateRequest reviewUpdateRequest = new ReviewUpdateRequest(1L,
+                5 ,
+                5 ,
+                5 ,
+                "comment");
+        User user = new User();
+        Review review = new Review(user,
+                "restaurantId",
+                5 ,
+                5 ,
+                5 ,
+                "comment",
+                LocalDate.now());
+    }
+}
---
+++ Final-Project/UserService/src/test/java/com/berkayarslan/UserService/controller/contract/impl/UserControllerContractImplTest.java
@@ -0,0 +1,188 @@
+package com.berkayarslan.UserService.controller.contract.impl;
+
+import com.berkayarslan.UserService.dto.UserDTO;
+import com.berkayarslan.UserService.dto.UserLocationDTO;
+import com.berkayarslan.UserService.mapper.UserMapper;
+import com.berkayarslan.UserService.model.User;
+import com.berkayarslan.UserService.request.UserSaveRequest;
+import com.berkayarslan.UserService.request.UserUpdateRequest;
+import com.berkayarslan.UserService.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserControllerContractImplTest {
+
+    @InjectMocks
+    private UserControllerContractImpl userControllerContract;
+
+    @Mock
+    private UserService userService;
+
+
+    @Test
+    void saveUser() {
+        UserSaveRequest userSaveRequest = new UserSaveRequest("firstName",
+                                                              "lastName",
+                                                              "0123456789",
+                                                              "example@gmail.com",
+                                                              "987456123",
+                                                              12.123456,
+                                                              12.123456);
+        User user = new User("firstName",
+                             "lastName",
+                             "0123456789",
+                             "example@gmail.com",
+                             "987456123",
+                             12.123456,
+                             12.123456);
+        User savedUser = new User(12L,
+                                  "firstName",
+                                  "lastName",
+                                  "0123456789",
+                                  "example@gmail.com",
+                                  "987456123",
+                                  12.123456,
+                                  12.123456);
+
+
+
+        when(userService.save(user)).thenReturn(savedUser);
+
+        UserDTO result = userControllerContract.saveUser(userSaveRequest);
+
+        assertNotNull(result);
+        verify(userService).save(user);
+    }
+
+    @Test
+    void findAllUsers() {
+
+        List<User> users = Arrays.asList(new User(12L,
+                                                  "firstName",
+                                                  "lastName",
+                                                  "0123456789",
+                                                  "example@gmail.com",
+                                                  "987456123",
+                                                  12.123456,
+                                                  12.123456));
+        List<UserDTO> userDTOs = Arrays.asList(new UserDTO(12L,
+                                                           "firstName",
+                                                           "lastName",
+                                                           "0123456789",
+                                                           "example@gmail.com",
+                                                           "987456123",
+                                                           12.123456,
+                                                           12.123456));
+
+        when(userService.findAll()).thenReturn(users);
+
+
+        List<UserDTO> result = userControllerContract.findAllUsers();
+
+        assertNotNull(result);
+        assertFalse(result.isEmpty());
+        verify(userService).findAll();
+
+
+    }
+
+    @Test
+    void getUserById() {
+
+        User user = new User();
+        UserDTO userDTO = new UserDTO(12L,
+                                      "firstName",
+                                      "lastName",
+                                      "0123456789",
+                                      "example@gmail.com",
+                                      "987456123",
+                                      12.123456,
+                                      12.123456);
+
+        when(userService.findByIdWithControl(1L)).thenReturn(user);
+
+        UserDTO result = userControllerContract.getUserById(1L);
+
+        assertNotNull(result);
+        verify(userService).findByIdWithControl(1L);
+    }
+
+    @Test
+    void getUserLocationById() {
+        User user = new User(12L, "firstName", "lastName", "0123456789", "example@gmail.com", "987456123", 12.123456, 12.123456);
+
+        when(userService.findByIdWithControl(1L)).thenReturn(user);
+
+        UserLocationDTO result = userControllerContract.getUserLocationById(1L);
+
+        assertNotNull(result);
+        assertEquals(12.123456, result.latitude());
+        assertEquals(12.123456, result.longitude());
+    }
+
+
+    @Test
+    void updateUser() {
+
+        UserUpdateRequest updateRequest = new UserUpdateRequest(1L,
+                                                                "firstName",
+                                                                "lastName",
+                                                                "0123456789",
+                                                                "example@gmail.com",
+                                                                "987456123",
+                                                                12.123456,
+                                                                12.123456);
+
+        User userBeforeUpdate = new User(1L,
+                                         "Name",
+                                         "SurName",
+                                         "0123456789",
+                                         "example@gmail.com",
+                                         "987456123",
+                                         12.123456,
+                                         12.123456);
+
+        User userAfterUpdate = new User(1L,
+                "firstName",
+                "lastName",
+                "0123456789",
+                "example@gmail.com",
+                "987456123",
+                12.123456,
+                12.123456);
+
+        UserDTO expectedUserDTO = new UserDTO(1L,
+                "firstName",
+                "lastName",
+                "0123456789",
+                "example@gmail.com",
+                "987456123",
+                12.123456,
+                12.123456);
+
+        when(userService.findByIdWithControl(1L)).thenReturn(userBeforeUpdate);
+        when(userService.save(userBeforeUpdate)).thenReturn(userAfterUpdate);
+
+        UserDTO resultUserDTO = userControllerContract.updateUser(updateRequest);
+
+        assertNotNull(resultUserDTO);
+
+        verify(userService).findByIdWithControl(updateRequest.id());
+        verify(userService).save(userBeforeUpdate);
+
+    }
+
+}
