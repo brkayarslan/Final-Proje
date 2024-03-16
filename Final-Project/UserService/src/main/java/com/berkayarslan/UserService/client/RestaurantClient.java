b524de258d8c879ad0cd350c57e1dc4c6273bca0
Berkay Arslan <berkayarslan9@gmail.com>
2024-03-16 09:17:53 +0300

    Add Restaurant Client sendUpdateRestaurantReview method

--- Final-Project/UserService/src/main/java/com/berkayarslan/UserService/client/RestaurantClient.java
+++ Final-Project/UserService/src/main/java/com/berkayarslan/UserService/client/RestaurantClient.java
@@ -2,11 +2,13 @@ package com.berkayarslan.UserService.client;
 
 
 import com.berkayarslan.UserService.dto.ReviewScoreDTO;
+import com.berkayarslan.UserService.dto.ReviewUpdateScoreDTO;
 import com.berkayarslan.UserService.general.RestResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(value = "restaurant", url = "http://localhost:8080/api/v1/restaurants")
@@ -14,4 +16,7 @@ public interface RestaurantClient {
 
     @PostMapping("/score/{id}")
     ResponseEntity<RestResponse<Boolean>> sendRestaurantScores(@PathVariable String id, @RequestBody ReviewScoreDTO reviewScoreDTO);
+
+    @PutMapping("/score/{id}")
+    ResponseEntity<RestResponse<Boolean>> sendUpdateRestaurantReview(@PathVariable String id, @RequestBody ReviewUpdateScoreDTO reviewUpdateScoreDTO);
 }
